50963021fbafdcedd474a18158fde35650e0f43a
Michael Ross <58342654+MichaelTRoss0@users.noreply.github.com>
2020-07-24 16:22:26 -0400

    Created classes for armors and accessories

---
+++ src/main/java/turnbasedrpg/entity/Accessory.java
@@ -0,0 +1,15 @@
+package turnbasedrpg.entity;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Michael T. Ross
+ *
+ */
+@Data
+public class Accessory extends Item {
+	
+	
+
+}
---
+++ src/main/java/turnbasedrpg/service/Action.java
@@ -0,0 +1,13 @@
+package turnbasedrpg.service;
+
+import turnbasedrpg.entity.Unit;
+
+public class Action {
+
+	public void Attack(Unit attacker, Unit defender) {
+		
+		
+		
+	}
+	
+}
